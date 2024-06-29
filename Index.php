4bee05e266c1942dca8e1e8f9b6f5b0ed71950c4
Soura47 <okiyama.i.t@gmail.com>
2024-06-29 19:24:43 -0300

    Hello

---
+++ Index.php
@@ -0,0 +1,24 @@
+<?php require_once
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form for="usuario">
+        <label for="usuario=>
+        <input type="text" name="usuario" Placeholder="salvar usuario">
+        <label for="Email=>
+        <input type="text" name="Email" Placeholder="salvar Email">
+        <label for=" enha=>
+        <input type="text" name="Senha" Placeholder="salvar Senha">
+        <button type="submit" name="Salvar"> Salvar  </button>
+    </form>
+</body>
+</html>
