a23367e4d098a0e8a0ae56695df4c80849fc10f7
Soura47 <okiyama.i.t@gmail.com>
2024-07-03 12:23:16 -0300

    A

--- Index.php
+++ Index.php
@@ -1,24 +1,42 @@
-<?php require_once
+<?php 
+session_start();
+include "Utils/Util.php";
+if (isset($_SESSION['user_name']) &&
+    isset($_SESSION['user_id'])) {
 
-
-
-?>
+	include "Controller/User.php";
+    $user->init($_SESSION['user_id']);
+    $user_data = $user->getUser();
+ ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home Page</title>
+	<link rel="stylesheet" 
+	      type="text/css" 
+	      href="Assets/css/style.css">
 </head>
 <body>
-    <form for="usuario">
-        <label for="usuario=>
-        <input type="text" name="usuario" Placeholder="salvar usuario">
-        <label for="Email=>
-        <input type="text" name="Email" Placeholder="salvar Email">
-        <label for=" enha=>
-        <input type="text" name="Senha" Placeholder="salvar Senha">
-        <button type="submit" name="Salvar"> Salvar  </button>
-    </form>
+   <div class="wrapper">
+    	<div class="form-holder">
+    		<h2>Welcome <?=$user_data['full_name']?> !</h2>
+    		<form class="form"
+    		      action="logout.php" 
+    		      method="GET">
+    		    <h4>Email: <?=$user_data['user_email']?> !</h4>
+    		    <h4>Username: <?=$user_data['user_name']?> !</h4>
+    			<div class="form-group">
+    				<button type="submit">Logout</button>
+    			</div>
+    		</form>
+    	</div>
+    </div>
 </body>
 </html>
+
+<?php }else { 
+$em = "First login ";
+Util::redirect("login.php", "error", $em);
+} ?>
